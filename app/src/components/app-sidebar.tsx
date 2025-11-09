ea40ed76f5882a489cb1057293c48248355de2be
krish-kant <krishna.200587@gmail.com>
2025-11-09 18:26:08 +0530

    chore: docs clean up and version bump

--- app/src/components/app-sidebar.tsx
+++ app/src/components/app-sidebar.tsx
@@ -229,7 +229,7 @@ const data = {
       title: "Docs",
       url: "https://github.com/supercheck-io/supercheck",
       icon: BookOpenText,
-      badge: "v1.1.6-beta.17",
+      badge: "v1.1.7-beta",
     },
     // {
     //   title: "Changelog",
