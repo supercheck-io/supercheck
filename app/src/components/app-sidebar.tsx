39d0dda25e95bc1e887ff3d86de8398cc2990e45
krish-kant <krishna.200587@gmail.com>
2025-12-04 13:16:26 +0530

    refactor(html-report-parser): remove unused import and implement dynamic import for JSDOM

--- app/src/components/app-sidebar.tsx
+++ app/src/components/app-sidebar.tsx
@@ -209,7 +209,7 @@ const data = {
       title: "Docs",
       url: "https://github.com/supercheck-io/supercheck",
       icon: BookOpenText,
-      badge: "v1.1.9-beta.29",
+      badge: "v1.1.9-beta.30",
     },
     // {
     //   title: "Changelog",
