7b26bcad6e47f703d17df51a06e91abbb89e418d
krish-kant <krishna.200587@gmail.com>
2025-12-10 15:46:29 +0530

    chore: bump version badge to v1.1.9-beta.49

--- app/src/components/app-sidebar.tsx
+++ app/src/components/app-sidebar.tsx
@@ -209,7 +209,7 @@ const data = {
       title: "Docs",
       url: "https://supercheck.io/docs",
       icon: BookOpenText,
-      badge: "v1.1.9-beta.48",
+      badge: "v1.1.9-beta.49",
     },
   ],
   documents: [
